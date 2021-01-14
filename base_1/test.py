bf074683781d26c96759287ca1357a91a7d334a5
58584@sangfor.com <58584@sangfor.com>
2021-01-14 21:46:13 +0800

    test.py

---
+++ base_1/test.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# Copyright Sangfor. All rights reserved
+
+'''
+测试脚本
+'''
+
+
+def main():
+    '''
+    主函数
+    :return:
+    '''
+
+    print('test main ......')
+
+
+if __name__ == '__main__':
+	main()
