f31bdcfe44868387cf1aba72e67af52574544673
58584@sangfor.com <58584@sangfor.com>
2021-01-22 15:50:02 +0800

    var

--- base_1/02_var.py
+++ base_1/02_var.py
@@ -1,12 +1,26 @@
 # coding=utf-8
-# hello word 脚本
-
+# python变量(变量使用、数字、  字符串、 字典、列表）
+#
+#
 def main():
     '''
     主函数
     :return:
     '''
-    print('hello word')
+    print('--------------')
+    var_int = 10
+    var_float = 10.0
+    var_long = 51924361L
+    var_complex = 3.14j
+    print('var_int : {}'.format(var_int))
+    print('var_float : {}'.format(var_float))
+    print('var_long : {}'.format(var_long))
+    print('var_complex : {}'.format(var_complex))
+
+    var_str = 'a'
+    var_str = 'this is string'
+    var_str = '中文字符串'
+
 
 
 if __name__ == '__main__':
